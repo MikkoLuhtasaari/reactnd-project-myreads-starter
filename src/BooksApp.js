c6b17a4fd4ea0545600423858e2b25a7490984de
John Doe <mikko.luhtasaari@cybercom.com>
2018-05-02 13:25:53 +0300

    Filter books according to their shelf status

--- src/BooksApp.js
+++ src/BooksApp.js
@@ -5,87 +5,101 @@ import Book from "./Book";
 import SearchBooks from './SearchBooks'
 import {Route, Link} from 'react-router-dom'
 
+const currentlyReading = "Currently Reading";
+const wantToRead = "Want to Read";
+const read = "Read";
+
 class BooksApp extends React.Component {
-  state = {
-    books : []
-  };
-  componentDidMount() {
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
         BooksAPI.getAll().then((books) => {
             this.setState(() => ({
                 books
             }))
         });
-  }
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Route path='/search' render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to='/'>Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    render() {
+        return (
+            <div className="app">
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                  <SearchBooks books={this.state.books}/>
-                  {this.state.books.map((book) =>(
-                            <Book key={book.id} book={book}></Book>
-                  ))}
-              </div>
+                <Route path='/search' render={() => (
+                    <div className="search-books">
+                        <div className="search-books-bar">
+                            <Link className="close-search" to='/'>Close</Link>
+                            <div className="search-books-input-wrapper">
+                                <input type="text" placeholder="Search by title or author"/>
+                                <SearchBooks books={this.state.books}/>
+                                {this.state.books.map((book) => (
+                                    <Book key={book.id} book={book}></Book>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="search-books-results">
+                            <ol className="books-grid"></ol>
+                        </div>
+                    </div>
+                )}/>
+
+                <Route exact path='/' render={() => (
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>MyReads</h1>
+                        </div>
+                        <div className="list-books-content">
+                            <div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Currently Reading</h2>
+                                    <div className="bookshelf-books">
+                                        <ol className="books-grid">
+                                            {this.state.books.filter((b) => (
+                                                b.shelf.toLowerCase().includes(currentlyReading.toLowerCase())
+                                            )).map((book) => (
+                                                <li><Book book={book}/></li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Want to Read</h2>
+                                    <div className="bookshelf-books">
+                                        <ol className="books-grid">
+                                            {this.state.books.filter((b) => (
+                                                b.shelf.toLowerCase().includes(wantToRead.toLowerCase())
+                                            )).map((book) => (
+                                                <li><Book book={book}/></li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </div>
+                                <div className="bookshelf">
+                                    <h2 className="bookshelf-title">Read</h2>
+                                    <div className="bookshelf-books">
+                                        <ol className="books-grid">
+                                            {this.state.books.filter((b) => (
+                                                b.shelf.toLowerCase().includes(read.toLowerCase())
+                                            )).map((book) => (
+                                                <li><Book book={book}/></li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="open-search">
+                            <Link
+                                to='/search'>
+                                Add a book
+                            </Link>
+                        </div>
+                    </div>
+                )}/>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        )}/>
-          <Route exact path='/' render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link
-                  to='/search' >
-                  Add a book
-              </Link>
-            </div>
-          </div>
-        )} />
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default BooksApp
